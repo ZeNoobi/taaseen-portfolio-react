834ef46a8fd6d8977cfe520b35af3348aa37134b
ZeNoobi <ctaaseen@gmail.com>
2023-08-18 00:40:41 -0700

    Responsive Changes

--- app/sections/Introduction.tsx
+++ app/sections/Introduction.tsx
@@ -2,23 +2,27 @@ import Image from 'next/image';
 
 const IntroPage = () => {
   return (
-    <div className='h-[900px] md:h-full w-full bg-lightnavy text-white flex justify-center md:items-center'>
+    <div className='h-[800px] md:h-full w-full bg-lightnavy text-white flex justify-center md:items-center'>
       {/*<section className='flex items-center justify-between'></section>*/}
       <div className=' flex justify-center min-w-full h-56 flex-wrap mt-32 md:mt-0'>
         <div className='w-[600px]'>
-          <h3 className='text-xl p-3'>
-            Hi, My name is
-            <span className='text-emerald-400'> Taaseen Chowdhury</span>.
-          </h3>
-          <h3 className='text-8xl lg:text-8xl'>
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-              Fullstack
-            </span>{' '}
-            Developer
-          </h3>
+          <div className='flex justify-center xl:justify-start xl:ml-5'>
+            <h3 className='sm:text-xl p-3'>
+              Hi, My name is
+              <span className='text-emerald-400'> Taaseen Chowdhury</span>.
+            </h3>
+          </div>
+          <div className='flex justify-center'>
+            <h3 className='text-4xl ml-3 mb-5 sm:text-5xl lg:text-6xl xl:text-8xl'>
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+                Fullstack
+              </span>{' '}
+              Developer
+            </h3>
+          </div>
         </div>
         {/*<div className='w-1/5'></div>*/}
-        <div className='w-[600px] flex justify-center items-center'>
+        <div className='w-[600px] flex justify-center items-center p-3'>
           <Image
             src='/UndrawCodeThinkingEmerald.svg'
             width={625}
--- app/sections/Technology.tsx
+++ app/sections/Technology.tsx
@@ -1,17 +1,19 @@
 const TechnologySection = () => {
   return (
-    <div className='w-full h-full bg-gradient-to-b from-indigo-950 to-indigo-950 text-white'>
-      <div className='flex justify-center'>
-        <h1>Technology I Use</h1>
-      </div>
-      <div className='w-full p-5 flex justify-evenly flex-wrap'>
-        <div className='h-20 w-[150px] bg-slate-500'>HTML</div>
-        <div className='h-20 w-[150px] bg-slate-500'>CSS</div>
-        <div className='h-20 w-[150px] bg-slate-500'>Javascript</div>
-        <div className='h-20 w-[150px] bg-slate-500'>React</div>
-        <div className='h-20 w-[150px] bg-slate-500'>TailwindCSS</div>
-        <div className='h-20 w-[150px] bg-slate-500'>NextJS</div>
-        <div className='h-20 w-[150px] bg-slate-500'>NodeJS</div>
+    <div className='w-full h-full bg-gradient-to-b from-indigo-950 to-indigo-950 text-white center flex justify-center items-center'>
+      <div className='w-full h-full'>
+        <div className='flex justify-center'>
+          <h1>Technology I Use</h1>
+        </div>
+        <div className='w-full p-5 flex justify-evenly flex-wrap'>
+          <div className='h-20 w-[150px] bg-slate-500'>HTML</div>
+          <div className='h-20 w-[150px] bg-slate-500'>CSS</div>
+          <div className='h-20 w-[150px] bg-slate-500'>Javascript</div>
+          <div className='h-20 w-[150px] bg-slate-500'>React</div>
+          <div className='h-20 w-[150px] bg-slate-500'>TailwindCSS</div>
+          <div className='h-20 w-[150px] bg-slate-500'>NextJS</div>
+          <div className='h-20 w-[150px] bg-slate-500'>NodeJS</div>
+        </div>
       </div>
     </div>
   );
