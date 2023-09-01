b9ce42680c2cd8ed121dd517af1a3b18b7c1d50c
ZeNoobi <ctaaseen@gmail.com>
2023-08-31 23:41:54 -0700

    proj mod

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -6,8 +6,13 @@ const ProjectSection = () => {
           What I&apos;ve Worked On
         </h2>
       </div>
-      <div className='bg-green-500 w-[1300px] h-80 m-5'>
+      <div className='bg-green-500 w-[1300px] h-80 m-5 p-5 flex'>
         <div className='h-full w-1/3 bg-gray-600'></div>
+        <div className='bg-red-500 w-full h-full'>
+          <div className='w-full bg-orange-700'>
+            <h3 className='text-5xl'>Project 1</h3>
+          </div>
+        </div>
       </div>
       <div className='bg-green-500 w-[1300px] h-80 m-5'>Project 2</div>
       <div className='bg-green-500 w-[1300px] h-80 m-5'>Project 3</div>
