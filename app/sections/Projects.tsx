e673243fde25667544a858cb42a53c0f5a16e162
ZeNoobi <ctaaseen@gmail.com>
2023-09-07 20:47:51 -0700

    footer

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -9,9 +9,28 @@ const ProjectSection = () => {
       <div className='bg-green-500 w-[1300px] h-80 m-5 p-5 flex'>
         <div className='h-full w-1/3 bg-gray-600'></div>
         <div className='bg-red-500 w-full h-full'>
-          <div className='w-full bg-orange-700'>
+          <div className='w-full bg-orange-700 h-1/4'>
             <h3 className='text-5xl'>Project 1</h3>
           </div>
+          <div className='h-2/4 bg-lime-500'>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+              ipsa veniam est enim consectetur doloribus obcaecati sed quasi,
+              provident id ut repellat consequatur nesciunt cum minima quaerat
+              maxime a velit. Repellendus similique sit non magni pariatur
+              laboriosam atque quod quisquam iste. Excepturi dolorum porro
+              voluptatibus quo suscipit modi, veniam nihil fugiat nisi molestias
+              itaque minima atque, quidem illum facere cum. Quod quasi error
+              totam voluptatibus optio, unde vero, ducimus aut officia eos harum
+              incidunt nobis modi minima similique sapiente esse nam accusamus
+              porro repellendus est fugiat cupiditate eligendi? Nisi, nobis?
+            </p>
+          </div>
+          <footer className='h-1/4 bg-green-600 w-full flex'>
+            <button>Visit</button>
+            <button>Github</button>
+            <button>Demo</button>
+          </footer>
         </div>
       </div>
       <div className='bg-green-500 w-[1300px] h-80 m-5'>Project 2</div>
